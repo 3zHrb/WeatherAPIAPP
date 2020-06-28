efbf9265571554a5924292e27a69617de12e94b5
Abdulaziz Alharbi <abdulazizalharbi@Abdulazizs-MacBook-Pro.local>
2020-06-28 22:37:05 +0300

    creating the initial files

---
+++ WeatherAPIAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAPIAPP
+//
+//  Created by Abdulaziz Alharbi on 07/11/1441 AH.
+//  Copyright © 1441 Abdulaziz Alharbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
